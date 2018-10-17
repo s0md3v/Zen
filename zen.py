92b510d868efbadf88153853f9091ef1700d6146
Somdev Sangwan <s0md3v@gmail.com>
2018-10-17 19:31:05 +0530

    Update zen.py

--- zen.py
+++ zen.py
@@ -1,17 +1,23 @@
 import re
 import json
 import argparse
+import threading
 from requests import get
+from requests.auth import HTTPBasicAuth
 
 parser = argparse.ArgumentParser()
 parser.add_argument('target', help='target')
 parser.add_argument('-o', help='output file', dest='output')
+parser.add_argument('-u', help='your username', dest='uname')
+parser.add_argument('-t', help='number of threads', dest='threads', type=int)
 parser.add_argument('--org', help='organization', dest='org', action='store_true')
 args = parser.parse_args()
 
 inp = args.target
 output = args.output
 organization = args.org
+uname = args.uname or ''
+thread_count = args.threads or 2
 
 end = '\033[1;m'
 green = '\033[1;32m'
@@ -26,6 +32,8 @@ if inp.endswith('/'):
 	inp = inp[:-1]
 
 targetOrganization = targetRepo = targetUser = False
+
+jsonOutput = {}
 
 if inp.count('/') < 4:
 	if '/' in inp:
@@ -46,12 +54,12 @@ else:
 	quit()
 
 def findContributorsFromRepo(username, repo):
-	response = get('https://api.github.com/repos/%s/%s/contributors?per_page=100' % (username, repo)).text
+	response = get('https://api.github.com/repos/%s/%s/contributors?per_page=100' % (username, repo), auth=HTTPBasicAuth(uname, '')).text
 	contributors = re.findall(r'https://github\.com/(.*?)"', response)
 	return contributors
 
 def findReposFromUsername(username):
-	response = get('https://api.github.com/users/%s/repos?per_page=100&sort=pushed' % username).text
+	response = get('https://api.github.com/users/%s/repos?per_page=100&sort=pushed' % username, auth=HTTPBasicAuth(uname, '')).text
 	repos = re.findall(r'"full_name":"%s/(.*?)",.*?"fork":(.*?),' % username, response)
 	nonForkedRepos = []
 	for repo in repos:
@@ -60,10 +68,16 @@ def findReposFromUsername(username):
 	return nonForkedRepos
 
 def findEmailFromContributor(username, repo, contributor):
-	response = get('https://github.com/%s/%s/commits?author=%s' % (username, repo, contributor)).text
-	latestCommit = re.search(r'href="/%s/%s/commit/(.*?)"' % (username, repo), response).group(1)
-	commitDetails = get('https://github.com/%s/%s/commit/%s.patch' % (username, repo, latestCommit)).text
-	email = re.search(r'<(.*)>', commitDetails).group(1)
+	response = get('https://github.com/%s/%s/commits?author=%s' % (username, repo, contributor), auth=HTTPBasicAuth(uname, '')).text
+	latestCommit = re.search(r'href="/%s/%s/commit/(.*?)"' % (username, repo), response)
+	if latestCommit:
+		latestCommit = latestCommit.group(1)
+	else:
+		latestCommit = 'dummy'
+	commitDetails = get('https://github.com/%s/%s/commit/%s.patch' % (username, repo, latestCommit), auth=HTTPBasicAuth(uname, '')).text
+	email = re.search(r'<(.*)>', commitDetails)
+	if email:
+		email = email.group(1)
 	return email
 
 def findEmailFromUsername(username):
@@ -72,32 +86,47 @@ def findEmailFromUsername(username):
 		email = findEmailFromContributor(username, repo, username)
 		if email:
 			print (username + ' : ' + email)
+			jsonOutput[username] = email
 			break
 
 def findEmailsFromRepo(username, repo):
 	contributors = findContributorsFromRepo(username, repo)
-	jsonOutput = {}
 	print ('%s Total contributors: %s%i%s' % (info, green, len(contributors), end))
+	flash(findEmailFromUsername, usernames)
 	for contributor in contributors:
 		email = (findEmailFromContributor(username, repo, contributor))
-		print (contributor + ' : ' + email)
-		jsonOutput[contributor] = email
-	if output:
-		json_string = json.dumps(jsonOutput, indent=4)
-		savefile = open(output, 'w+')
-		savefile.write(json_string)
-		savefile.close()
 
 def findUsersFromOrganization(username):
-	response = get('https://api.github.com/orgs/%s/members?per_page=100' % username).text
+	response = get('https://api.github.com/orgs/%s/members?per_page=100' % username, auth=HTTPBasicAuth(uname, '')).text
 	members = re.findall(r'"login":"(.*?)"', response)
 	return members
 
+def threader(function, arg):
+    threads = []
+    for i in arg:
+        task = threading.Thread(target=function, args=(i,))
+        threads.append(task)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    del threads[:]
+
+def flash(function, arg):
+    for begin in range(0, len(arg), thread_count):
+        end = begin + thread_count
+        splitted = arg[begin:end]
+        threader(function, splitted)
+
 if targetOrganization:
 	usernames = findUsersFromOrganization(username)
-	for username in usernames:
-		findEmailFromUsername(username)
+	flash(findEmailFromUsername, usernames)
 elif targetUser:
 	findEmailFromUsername(username)
 elif targetRepo:
 	findEmailsFromRepo(username, repo)
+if output:
+	json_string = json.dumps(jsonOutput, indent=4)
+	savefile = open(output, 'w+')
+	savefile.write(json_string)
+	savefile.close()
